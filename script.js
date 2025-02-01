8c81392ecd23f99325e3b98086d1bab9ce44bd43
russkibrokeboy <kirillfadeeff@gmail.com>
2025-02-01 21:04:08 +0400

    Update script.js
    .

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function capsUpdate(event) {
 function generateURL(event) {
     event.preventDefault();
 
-    const generatedUrl = 'https://russkibrokeboy.github.io/jchatkick1/v2/index.html?channel=' + $channel.val();
+    const generatedUrl = 'https://russkibrokeboy.github.io/jchatKick1/v2/index.html?channel=' + $channel.val();
 
     let data = {
         size: $size.val(),
