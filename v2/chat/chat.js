49e73d92bfb086d52b2a3e959b4e5c63b14b7d33
russkibrokeboy <kirillfadeeff@gmail.com>
2025-02-02 22:28:59 +0400

    .

--- v2/chat/chat.js
+++ v2/chat/chat.js
@@ -2,79 +2,149 @@ class Chat {
     #info = {};
     static #baseUrl = "wss://ws-us2.pusher.com/app/32cbd69e4b950bf97679";
 
-    constructor(user, maxMessages, animate, fade, showBadges, hideCommands, hideBots, externalCss) {
-        this.user = user;
-        this.maxMessages = maxMessages;
-        this.animate = animate === "true";
-        this.fade = fade;
-        this.showBadges = showBadges === "true";
-        this.hideCommands = hideCommands === "true";
-        this.hideBots = hideBots === "true";
-        this.externalCss = externalCss;
-        this.#info = { lines: [] };
-        
-        this.getUserInfoWithRetry();
-        this.update();
+    constructor(info) {
+        this.#info = info;
+        this.#load().then(() => this.#connect());
+        this.#update();
     }
 
-    async getUserInfoWithRetry() {
-        try {
-            const userData = await this.getUserInfo(this.user);
-            await this.getSubBadgesAndEmotes(userData);
-            console.log("Chatroom ID:", userData.chatroom.id);
-            this.connectToChatroom(userData.chatroom.id, userData.chatroom.channel_id);
-        } catch (error) {
-            console.error("Error:", error);
-            setTimeout(() => this.getUserInfoWithRetry(), 1000);
+    async #load() {
+        const res = await (await fetch('https://kick.com/api/v2/channels/' + this.#info.channel)).json();
+
+        this.#info.channelID = res.id;
+        this.#info.chatRoomId = res.chatroom.id;
+        await EmotesRepository.load();
+
+        let size = sizes[ChatOptions.size - 1];
+        let font = fonts[ChatOptions.font];
+
+        appendCSS('size', size);
+        appendCSS('font', font);
+
+        if (ChatOptions.stroke && ChatOptions.stroke > 0) {
+            let stroke = strokes[ChatOptions.stroke - 1];
+            appendCSS('stroke', stroke);
+        }
+        if (ChatOptions.shadow && ChatOptions.shadow > 0) {
+            let shadow = shadows[ChatOptions.shadow - 1];
+            appendCSS('shadow', shadow);
+        }
+        if (ChatOptions.smallCaps) {
+            appendCSS('variant', 'SmallCaps');
+        }
+
+        BadgesRepository.load([...res.subscriber_badges, ...res.follower_badges]);
+        this.#info.contentLoaded = true;
+    }
+
+    #update() {
+        setInterval(() => {
+            const chatLine = $('.chat_line');
+            if (this.#info.lines.length) {
+                const lines = this.#info.lines.join('');
+
+                if (ChatOptions.animate) {
+                    const $auxDiv = $('<div></div>', {class: "hidden"}).appendTo("#chat_container");
+                    $auxDiv.append(lines);
+                    const auxHeight = $auxDiv.height();
+                    $auxDiv.remove();
+
+                    const $animDiv = $('<div></div>');
+                    $('#chat_container').append($animDiv);
+                    $animDiv.animate({"height": auxHeight}, 150, function () {
+                        $(this).remove();
+                        $('#chat_container').append(lines);
+                    });
+                } else {
+                    $('#chat_container').append(lines);
+                }
+                this.#info.lines = [];
+
+                for (let i = chatLine.length - 100; i > 0; i--) {
+                    chatLine.eq(0).remove();
+                }
+
+            } else if (ChatOptions.fade) {
+                const messageTime = chatLine.eq(0).data('time');
+                if ((Date.now() - messageTime) / 1000 >= ChatOptions.fade) {
+                    chatLine.eq(0).fadeOut(function () {
+                        $(this).remove();
+                    });
+                }
+            }
+        }, 200)
+    }
+
+    #onMessageDeletedHandler(id) {
+        $(`div[data-id=${id}]`).remove();
+    }
+
+    #onUserBannedHandler(id) {
+        $(`div[data-user-id=${id}]`).remove();
+    }
+
+    async #connect(channel) {
+        if (!this.#info.contentLoaded) {
+            this.#info.channel = channel;
+            const title = $(document).prop('title');
+            $(document).prop('title', title + this.#info.channel);
+
+            await this.#load();
+        }
+
+        console.log('Connecting...');
+        const urlParams = new URLSearchParams({
+            protocol: "7",
+            client: "js",
+            version: "8.4.0-rc2",
+            flash: false,
+        });
+
+        const url = `${this.constructor.#baseUrl}?${urlParams.toString()}`;
+
+        const socket = new WebSocket(url);
+
+        socket.onopen = () => {
+            socket.send(JSON.stringify({
+                event: "pusher:subscribe",
+                data: {auth: "", channel: `chatrooms.${this.#info.chatRoomId}.v2`},
+            }));
+
+            console.log('connected');
+        };
+
+        socket.onclose = () => {
+            console.log('Disconnected');
+            setTimeout(this.#connect, 1e3 * 3, this.#info.channel);
+        };
+
+        socket.onmessage = (messageEvent) => {
+            const messageEventJSON = JSON.parse(messageEvent.data.toString());
+            let data = {};
+
+            if (typeof messageEventJSON.data === 'string') {
+                data = JSON.parse(messageEventJSON.data);
+            }
+
+            switch (messageEventJSON.event) {
+                case "App\\Events\\ChatMessageEvent":
+                    this.#onMessageHandler(data);
+
+                    break;
+                case "App\\Events\\MessageDeletedEvent":
+                    this.#onMessageDeletedHandler(data.message.id);
+
+                    break;
+                case "App\\Events\\UserBannedEvent":
+                    this.#onUserBannedHandler(data.user.id);
+
+                    break;
+            }
         }
     }
 
-    connectToChatroom(chatroomID, channelID) {
-        const chat = new WebSocket(
-            `${Chat.#baseUrl}?protocol=7&client=js&version=8.4.0-rc2&flash=false`
-        );
-
-        chat.onerror = (error) => {
-            console.error("WebSocket Error:", error);
-        };
-
-        chat.onopen = () => {
-            console.log("Connected to Pusher");
-            document.getElementById("loading").innerHTML = "Connected";
-            setTimeout(() => {
-                document.getElementById("loading").style.display = "none";
-            }, 1500);
-            this.subscribeToChannel(chat, `chatrooms.${chatroomID}.v2`);
-            this.subscribeToChannel(chat, `channel.${channelID}`);
-        };
-
-        chat.onmessage = (event) => {
-            this.parseMessage(event.data);
-        };
+    #onMessageHandler(data) {
+        const chatMessage = new ChatMessage(new ChatMessageData(data));
+        this.#info.lines.push(chatMessage.toHtml());
     }
 }
-
-// Make Chat available globally
-window.Chat = Chat;
-
-// script.js
-$(document).ready(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const pageSetup = new PageSetup(urlParams);
-
-    if (!urlParams.get("user")) {
-        window.location.replace("/");
-    } else {
-        const chat = new Chat(
-            urlParams.get("user"),
-            pageSetup.maxMessages,
-            urlParams.get("animate"),
-            urlParams.get("fade"),
-            urlParams.get("badges"),
-            urlParams.get("commands"),
-            urlParams.get("bots"),
-            urlParams.get("external-css")
-        );
-        pageSetup.applyCustomisation();
-    }
-});
