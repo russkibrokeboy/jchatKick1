1858873b6e717201d54088ec28b9b69908abd834
russkibrokeboy <kirillfadeeff@gmail.com>
2025-02-02 20:13:16 +0400

    .

--- script.js
+++ script.js
@@ -1,174 +1,193 @@
-function fadeOption(event) {
-    if ($fade_bool.is(':checked')) {
-        $fade.removeClass('hidden');
-        $fade_seconds.removeClass('hidden');
-    } else {
-        $fade.addClass('hidden');
-        $fade_seconds.addClass('hidden');
+$(document).ready(() => {
+    // jQuery element declarations
+    const $fade_bool = $('#fade_bool');
+    const $fade = $('#fade');
+    const $fade_seconds = $('#fade_seconds');
+    const $size = $('#size');
+    const $font = $('#font');
+    const $stroke = $('#stroke');
+    const $shadow = $('#shadow');
+    const $badges = $('#badges');
+
+    // Helper functions
+    function appendCSS(type, value) {
+        console.log(`Appending CSS: ${type}=${value}`);
     }
-}
 
-function sizeUpdate(event) {
-    let size = sizes[Number($size.val()) - 1];
-    removeCSS('size');
-    appendCSS('size', size);
-}
+    function removeCSS(type) {
+        console.log(`Removing CSS: ${type}`);
+    }
 
-function fontUpdate(event) {
-    let font = fonts[Number($font.val())];
-    removeCSS('font');
-    appendCSS('font', font);
-}
+    // Event handlers
+    function fadeOption(event) {
+        if ($fade_bool.is(':checked')) {
+            $fade.removeClass('hidden');
+            $fade_seconds.removeClass('hidden');
+        } else {
+            $fade.addClass('hidden');
+            $fade_seconds.addClass('hidden');
+        }
+    }
 
-function strokeUpdate(event) {
-    removeCSS('stroke');
-    if ($stroke.val() == "0")
-        return;
-    else {
+    function sizeUpdate(event) {
+        let size = sizes[Number($size.val()) - 1];
+        removeCSS('size');
+        appendCSS('size', size);
+    }
+
+    function fontUpdate(event) {
+        let font = fonts[Number($font.val())];
+        removeCSS('font');
+        appendCSS('font', font);
+    }
+
+    function strokeUpdate(event) {
+        removeCSS('stroke');
+        if ($stroke.val() == "0") return;
         let stroke = strokes[Number($stroke.val()) - 1];
         appendCSS('stroke', stroke);
     }
-}
 
-function shadowUpdate(event) {
-    removeCSS('shadow');
-    if ($shadow.val() == "0")
-        return;
-    else {
+    function shadowUpdate(event) {
+        removeCSS('shadow');
+        if ($shadow.val() == "0") return;
         let shadow = shadows[Number($shadow.val()) - 1];
         appendCSS('shadow', shadow);
     }
-}
 
-function badgesUpdate(event) {
-    if ($badges.is(':checked')) {
-        $('img[class="badge special"]').addClass('hidden');
-    } else {
-        $('img[class="badge special hidden"]').removeClass('hidden');
+    function badgesUpdate(event) {
+        if ($badges.is(':checked')) {
+            $('img[class="badge special"]').addClass('hidden');
+        } else {
+            $('img[class="badge special hidden"]').removeClass('hidden');
+        }
     }
-}
 
-function capsUpdate(event) {
-    if ($small_caps.is(':checked')) {
-        appendCSS('variant', 'SmallCaps');
-    } else {
-        removeCSS('variant');
+    function capsUpdate(event) {
+        if ($small_caps.is(':checked')) {
+            appendCSS('variant', 'SmallCaps');
+        } else {
+            removeCSS('variant');
+        }
     }
-}
 
-function generateURL(event) {
-    event.preventDefault();
+    function generateURL(event) {
+        event.preventDefault();
 
-    const generatedUrl = 'https://russkibrokeboy.github.io/jchatKick1/v2/index.html?channel=' + $channel.val();
+        const generatedUrl = 'https://russkibrokeboy.github.io/jchatKick1/v2/index.html?channel=' + $channel.val();
 
-    let data = {
-        size: $size.val(),
-        font: $font.val(),
-        stroke: ($stroke.val() != '0' ? $stroke.val() : false),
-        shadow: ($shadow.val() != '0' ? $shadow.val() : false),
-        bots: $bots.is(':checked'),
-        hide_commands: $commands.is(':checked'),
-        hide_badges: $badges.is(':checked'),
-        animate: $animate.is(':checked'),
-        fade: ($fade_bool.is(':checked') ? $fade.val() : false),
-        small_caps: $small_caps.is(':checked'),
-        show_replies: $show_replies.is(':checked')
-    };
+        let data = {
+            size: $size.val(),
+            font: $font.val(),
+            stroke: ($stroke.val() != '0' ? $stroke.val() : false),
+            shadow: ($shadow.val() != '0' ? $shadow.val() : false),
+            bots: $bots.is(':checked'),
+            hide_commands: $commands.is(':checked'),
+            hide_badges: $badges.is(':checked'),
+            animate: $animate.is(':checked'),
+            fade: ($fade_bool.is(':checked') ? $fade.val() : false),
+            small_caps: $small_caps.is(':checked'),
+            show_replies: $show_replies.is(':checked')
+        };
 
-    const params = encodeQueryData(data);
+        const params = encodeQueryData(data);
 
-    $url.val(generatedUrl + '&' + params);
+        $url.val(generatedUrl + '&' + params);
 
-    $generator.addClass('hidden');
-    $result.removeClass('hidden');
-}
-
-function changePreview(event) {
-    if ($example.hasClass("white")) {
-        $example.removeClass("white");
-        $brightness.attr('src', "img/light.png");
-    } else {
-        $example.addClass("white");
-        $brightness.attr('src', "img/dark.png");
+        $generator.addClass('hidden');
+        $result.removeClass('hidden');
     }
-}
 
-function copyUrl(event) {
-    navigator.clipboard.writeText($url.val());
+    function changePreview(event) {
+        if ($example.hasClass("white")) {
+            $example.removeClass("white");
+            $brightness.attr('src', "img/light.png");
+        } else {
+            $example.addClass("white");
+            $brightness.attr('src', "img/dark.png");
+        }
+    }
 
-    $alert.css('visibility', 'visible');
-    $alert.css('opacity', '1');
-}
+    function copyUrl(event) {
+        navigator.clipboard.writeText($url.val());
 
-function showUrl(event) {
-    $alert.css('opacity', '0');
-    setTimeout(function() {
-        $alert.css('visibility', 'hidden');
-    }, 200);
-}
+        $alert.css('visibility', 'visible');
+        $alert.css('opacity', '1');
+    }
 
-function resetForm(event) {
-    $channel.val('');
-    $size.val('3');
-    $font.val('0');
-    $stroke.val('0');
-    $shadow.val('0');
-    $bots.prop('checked', false);
-    $commands.prop('checked', false);
-    $badges.prop('checked', false);
-    $animate.prop('checked', false);
-    $fade_bool.prop('checked', false);
-    $fade.addClass('hidden');
-    $fade_seconds.addClass('hidden');
-    $fade.val("30");
-    $small_caps.prop('checked', false);
-    $show_replies.prop('checked', false);
+    function showUrl(event) {
+        $alert.css('opacity', '0');
+        setTimeout(function() {
+            $alert.css('visibility', 'hidden');
+        }, 200);
+    }
 
-    sizeUpdate();
-    fontUpdate();
-    strokeUpdate();
-    shadowUpdate();
-    badgesUpdate();
-    capsUpdate();
-    if ($example.hasClass("white"))
-        changePreview();
+    function resetForm(event) {
+        $channel.val('');
+        $size.val('3');
+        $font.val('0');
+        $stroke.val('0');
+        $shadow.val('0');
+        $bots.prop('checked', false);
+        $commands.prop('checked', false);
+        $badges.prop('checked', false);
+        $animate.prop('checked', false);
+        $fade_bool.prop('checked', false);
+        $fade.addClass('hidden');
+        $fade_seconds.addClass('hidden');
+        $fade.val("30");
+        $small_caps.prop('checked', false);
+        $show_replies.prop('checked', false);
 
-    $result.addClass('hidden');
-    $generator.removeClass('hidden');
-    showUrl();
-}
+        sizeUpdate();
+        fontUpdate();
+        strokeUpdate();
+        shadowUpdate();
+        badgesUpdate();
+        capsUpdate();
+        if ($example.hasClass("white"))
+            changePreview();
 
-const $generator = $("form[name='generator']");
-const $channel = $('input[name="channel"]');
-const $animate = $('input[name="animate"]');
-const $bots = $('input[name="bots"]');
-const $fade_bool = $("input[name='fade_bool']");
-const $fade = $("input[name='fade']");
-const $fade_seconds = $("#fade_seconds");
-const $commands = $("input[name='commands']");
-const $small_caps = $("input[name='small_caps']");
-const $badges = $("input[name='badges']");
-const $size = $("select[name='size']");
-const $font = $("select[name='font']");
-const $stroke = $("select[name='stroke']");
-const $shadow = $("select[name='shadow']");
-const $brightness = $("#brightness");
-const $example = $('#example');
-const $result = $("#result");
-const $url = $('#url');
-const $alert = $("#alert");
-const $reset = $("#reset");
-const $show_replies = $('input[name="show_replies"]');
+        $result.addClass('hidden');
+        $generator.removeClass('hidden');
+        showUrl();
+    }
 
-$fade_bool.change(fadeOption);
-$size.change(sizeUpdate);
-$font.change(fontUpdate);
-$stroke.change(strokeUpdate);
-$shadow.change(shadowUpdate);
-$small_caps.change(capsUpdate);
-$badges.change(badgesUpdate);
-$generator.submit(generateURL);
-$brightness.click(changePreview);
-$url.click(copyUrl);
-$alert.click(showUrl);
-$reset.click(resetForm);
+    // Event bindings
+    $fade_bool.on('change', fadeOption);
+    $size.on('change', sizeUpdate);
+    $font.on('change', fontUpdate);
+    $stroke.on('change', strokeUpdate);
+    $shadow.on('change', shadowUpdate);
+    $badges.on('change', badgesUpdate);
+    $small_caps.change(capsUpdate);
+    $generator.submit(generateURL);
+    $brightness.click(changePreview);
+    $url.click(copyUrl);
+    $alert.click(showUrl);
+    $reset.click(resetForm);
+
+    // Initialize chat if Chat class is available
+    if (typeof Chat !== 'undefined') {
+        const urlParams = new URLSearchParams(window.location.search);
+        const pageSetup = new PageSetup(urlParams);
+
+        if (!urlParams.get("user")) {
+            window.location.replace("/");
+        } else {
+            const chat = new Chat(
+                urlParams.get("user"),
+                pageSetup.maxMessages,
+                urlParams.get("animate"),
+                urlParams.get("fade"),
+                urlParams.get("badges"),
+                urlParams.get("commands"),
+                urlParams.get("bots"),
+                urlParams.get("external-css")
+            );
+            pageSetup.applyCustomisation();
+        }
+    } else {
+        console.error('Chat class not loaded');
+    }
+});
